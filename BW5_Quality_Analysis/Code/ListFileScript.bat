5fa60f7af06545af9e311c101964be7e746a8a85
Hussain <mohammed.hussain6@hp.com>
2018-08-19 08:26:43 -0500

    Quality Analysis code

---
+++ BW5_Quality_Analysis/Code/ListFileScript.bat
@@ -0,0 +1,2 @@
+cd C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Sample\Code
+dir /s/b *.process > C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Quality_Analysis\Code\myProcessList.txt
