357b372bef93fee47a744325167aeb84be11671e
Mohammed Hussain <shahzadhussain61224@gmail.com>
2018-08-28 01:25:53 -0500

    removing unwanted files

--- BW5_Quality_Analysis/Code/ListGVFileScript.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Sample\Code
-dir /s/b *.substvar > C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Quality_Analysis\Code\myGVList.txt
--- BW5_Quality_Analysis/Code/ListProcessFileScript.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Sample\Code
-dir /s/b *.process > C:\Users\mohammhu\Documents\myGitHub\github-tibco\BW5_Quality_Analysis\Code\myProcessList.txt
